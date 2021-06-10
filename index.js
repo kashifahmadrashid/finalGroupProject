4d91342d7512a9c4ae7366440d61a0a8d0775cda
Kashif Rashid <kashif.rashid84@gmail.com>
2021-06-10 11:21:27 +1000

    Sprint 2 Adding Index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+let taskTitle = document.querySelector("#taskTitle").value;
+let taskDescription = document.querySelector("#taskDescription").value;
+let taskAssignment = document.querySelector("#taskAssignment").value;
+let dueDate = document.querySelector("#taskDueDate").value;
+let taskStatus = document.querySelector("#inputState").value;
+let errMsg = document.querySelector("#errMsg1");
+let submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", validFormInput );
+
+function validFormInput () {
+    if(taskTitle == "" || taskTitle.length < 5){
+        errMsg.innerHTML = "The Title shouldn\'t be less than 5 characters"
+        document.querySelector("#errMsg1").style.color = "#ff0000";
+        taskTitle.focus(); // it will focus on the Task title input
+    }else{
+        errMsg.innerHTML = "OK to Submit";
+    }
+}
+
