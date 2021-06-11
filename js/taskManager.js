e8e20171ab628aa1a65aee100ad28d7828dabf92
Kashif Rashid <kashif.rashid84@gmail.com>
2021-06-11 10:16:56 +1000

    adding updates

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,14 +1,18 @@
 class TaskManager{
-    constructor(taskTitle, description, assingment, dueDate, status){
+    constructor(taskTitle, description, assignment, dueDate, status){
+        
         this.task = taskTitle;
         this.description = description;
         this.assignment = assignment;
         this.dueDate = dueDate;
         this.status = status;
+        
     }
 
 }
-
+const arrayTest = [];
+const newTask = new TaskManager('dinner', 'dinner with friends', 'kashif', 12/6/2021, 'in Progress' )
+console.log(arrayTest.push(newTask));
 // const taskList = new TaskManager();
 // console.log(taskList)
 // console.log(taskList.tasks)
