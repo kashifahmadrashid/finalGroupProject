345b4789bbb37aa52ecf7cd5b8696c507b9b5f71
vinh1 <vinhtieu98@gmail>
2021-06-15 17:55:16 +1000

    index.html adn taskManager update

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,14 +1,31 @@
-class TaskManager{
-    constructor(taskTitle, description, assingment, dueDate, status){
-        this.task = taskTitle;
-        this.description = description;
-        this.assignment = assignment;
-        this.dueDate = dueDate;
-        this.status = status;
+let taskTitle = document.querySelector("#taskTitle");
+let taskDescription = document.querySelector("#taskDescription");
+let taskAssignment = document.querySelector("#taskAssignment");
+let dueDate = document.querySelector("#taskDueDate");
+let taskStatus = document.querySelector("#inputState");
+let addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", validFormInput );
+
+class taskManager {
+    constructor (currentId = 0) {
+        this.tasksArray = [];
+        this.currentId = currentId;
     }
+    
+    addTask (id, name1, dcript, assign) {
+        class taskData {
+            constructor (id, name, dcript, assign) {
+                this.id = id + 1 ;
+                this.nameT = name;
+                this.dcripT = dcript;
+                this.assignT = assign;
+            }
+        }
+        this.taskObject = new taskData(this.currentId, name1, dcript, assign);
+        this.tasksArray.push(this.taskObject);
+        this.currentId = this.currentId + 1;
+    };
+};
 
-}
-
-// const taskList = new TaskManager();
-// console.log(taskList)
-// console.log(taskList.tasks)
+let taskManagerObj = new taskManager();
+let array1 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
--- taskManager.js
+++ taskManager.js
@@ -1,37 +1,37 @@
-class TaskManager {
+class taskManager {
     constructor (currentId = 0) {
+        this.tasksArray = [];
         this.currentId = currentId;
-        this.tasks = [];
     }
     
-    addTask (name, dcrip, assign) {
-        this.nameT = name;
-        this.dcripT = dcrip;
-        this.assignT = assign;
-        this.currentId ++;
-       
-    };
-    putIntoArray () {
-        //this.tasks = new Array; 
-        //var array1 = new Array()
-        // this.tasks.push(task1);
-        //this.tasks = array1;
-        
+    addTask (id, name1, dcript, assign) {
+        class taskData {
+            constructor (id, name, dcript, assign) {
+                this.id = id + 1 ;
+                this.nameT = name;
+                this.dcripT = dcript;
+                this.assignT = assign;
+            }
+        }
+        this.taskObject = new taskData(this.currentId, name1, dcript, assign);
+        this.tasksArray.push(this.taskObject);
+        this.currentId = this.currentId + 1;
     };
 };
 
+let taskManagerObj = new taskManager();
+let array1 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
+let array2 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
+let array3 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
+let array4 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
+// function array1() {
+//  if()
+// }
+console.log(taskManagerObj.taskObject.nameT);
+console.log(taskManagerObj.taskObject.dcripT);
+console.log(taskManagerObj.taskObject.assignT);
 
-const obj = new TaskManager();
-let arr1 = [];
-obj.addTask("HomeSite", "tastofvinh", "vinh");        
-var newtast = obj.addTask("HomeSite", "tastofvinh", "vinh"); 
-// obj.putIntoArray(newtast);
-console.log(obj.nameT);
-console.log(obj.dcripT);
-console.log(obj.assignT);
-
-//console.log(obj.addTask("HomeSite", "tastofvinh", "vinh"));
-arr1.push(newtast);
-console.log(obj.currentId);
-console.log(arr1[0]);
-//console.log(array1)
+console.log(taskManagerObj.tasksArray);
+// console.log(taskManagerObj.currentId);
+// console.log(taskManagerObj.taskObject.id)
+// console.log(taskManagerObj.tasksArray[0].taskData.nameT);
