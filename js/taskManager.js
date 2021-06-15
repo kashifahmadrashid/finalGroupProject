4ded28ce2082bc12e9fd7656523c8c60d5c06922
vinh1 <vinhtieu98@gmail>
2021-06-15 19:15:49 +1000

    index.html and inde.js and taskManager update

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,31 +1,64 @@
-let taskTitle = document.querySelector("#taskTitle");
-let taskDescription = document.querySelector("#taskDescription");
-let taskAssignment = document.querySelector("#taskAssignment");
-let dueDate = document.querySelector("#taskDueDate");
-let taskStatus = document.querySelector("#inputState");
-let addBtn = document.querySelector("#addBtn");
-addBtn.addEventListener("click", validFormInput );
-
-class taskManager {
-    constructor (currentId = 0) {
-        this.tasksArray = [];
-        this.currentId = currentId;
-    }
-    
-    addTask (id, name1, dcript, assign) {
-        class taskData {
-            constructor (id, name, dcript, assign) {
-                this.id = id + 1 ;
-                this.nameT = name;
-                this.dcripT = dcript;
-                this.assignT = assign;
-            }
-        }
-        this.taskObject = new taskData(this.currentId, name1, dcript, assign);
-        this.tasksArray.push(this.taskObject);
-        this.currentId = this.currentId + 1;
-    };
+const createTaskHTML = (taskTitle, taskDescription, taskAssignment, taskDueDate, inputState) =>{
+    const html = `<li class="list-group-item">
+                <div class="card">
+                  <div class="card-body d-flex w-100 justify-content-between">
+                        <div>
+                          <h5 class="card-title" id="taskHeading" >${taskTitle}</h5>
+                          <textarea class="form-control" id="taskDescription">${taskDescription}</textarea>
+                          <p class="card-text" id="taskAssign">${taskAssignment}</p>
+                          
+                        </div>
+                  <div>
+                  <div>
+                        <p class="card-text">${taskDueDate}</p>
+                        <p class="card-text">${inputState}</p>
+                  </div>
+                  <div class="mt-3">
+                        <a href="#" class="btn btn-primary">Details</a>
+                        <a href="#" class="btn btn-danger">Delete</a>
+                  </div>
+                </div>
+               </li> `;
+              return html;
 };
 
-let taskManagerObj = new taskManager();
-let array1 = taskManagerObj.addTask(taskManagerObj.currentId, "aa", "bb", "cc")
+class TaskManager{
+
+    constructor(currentId = 0){
+        
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+   
+    
+    addTask (taskTitle, taskDescription, taskAssignment, taskDueDate, inputState){
+        const newTask = {
+            newId: this.currentId++,
+            taskTitle: taskTitle,
+            taskDescription: taskDescription,
+            taskAssignment: taskAssignment,
+            taskDueDate: taskDueDate,
+            inputState: inputState,
+        };
+        this.tasks.push(newTask);
+        console.log(this.tasks)
+        return this.tasks;
+
+    };
+    render (){
+        const taskHtmlList = [];
+        for (let i=0; i<this.tasks.length; i++){
+            const task = this.tasks[i];
+            const date = new Date (task.taskDueDate);
+            const formattedDate = 
+            date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+            const taskHtml = createTaskHTML (task.taskTitle, task.taskDescription, task.taskAssignment, formattedDate, task.inputState);
+            taskHtmlList.push(taskHtml);
+        }
+            const tasksHtml = taskHtmlList.join("\n");
+            
+            const tasksList = document.querySelector("#task-list");
+            
+            tasksList.innerHTML = tasksHtml;
+        }
+}
