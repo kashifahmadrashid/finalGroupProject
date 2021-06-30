d9d9529180cef847b2f2e0eb057205c5c50cf621
vinh1 <vinhtieu98@gmail>
2021-06-30 10:48:06 +1000

    completely finish the all task as well

--- js/index.js
+++ js/index.js
@@ -23,6 +23,7 @@ function validFormInput (e) {
     var GivenDate = taskDueDate.value;
     var CurrentDate = new Date();
     GivenDate = new Date(GivenDate);
+    //CurrentDate = CurrentDate.
     
     if(taskTitle.value.trim() == "" || taskTitle.value.length < 5){
         errMsg1.innerHTML = "The Title shouldn\'t be less than 5 characters";
@@ -133,7 +134,7 @@ taskList0.addEventListener("click",(event) => {
         const taskId = Number(parentTask.dataset.taskId);
            // Delete the task
             taskManager.deleteTask(taskId);
-            location.reload()
+            // location.reload()
             // Save the tasks to localStorage
             taskManager.save();
             // Render the tasks
@@ -161,7 +162,7 @@ taskList1.addEventListener("click",(event) => {
         const taskId = Number(parentTask.dataset.taskId);
            // Delete the task
             taskManager.deleteTask(taskId);
-            location.reload()
+            // location.reload()
             // Save the tasks to localStorage
             taskManager.save();
             // Render the tasks
@@ -192,7 +193,7 @@ taskList1.addEventListener("click",(event) => {
             const taskId = Number(parentTask.dataset.taskId);
                // Delete the task
                 taskManager.deleteTask(taskId);
-                location.reload()
+                // location.reload()
                 // Save the tasks to localStorage
                 taskManager.save();
                 // Render the tasks
@@ -223,7 +224,7 @@ taskList1.addEventListener("click",(event) => {
                 const taskId = Number(parentTask.dataset.taskId);
                    // Delete the task
                     taskManager.deleteTask(taskId);
-                    location.reload()
+                    // location.reload()
                     // Save the tasks to localStorage
                     taskManager.save();
                     // Render the tasks
@@ -255,7 +256,7 @@ taskList1.addEventListener("click",(event) => {
                 const taskId = Number(parentTask.dataset.taskId);
                    // Delete the task
                     taskManager.deleteTask(taskId);
-                    location.reload()
+                    // location.reload()
                     // Save the tasks to localStorage
                     taskManager.save();
                     // Render the tasks
@@ -263,46 +264,3 @@ taskList1.addEventListener("click",(event) => {
               }
             });
 
-// check date function start here
-
-// function isDate(ExpiryDate) { 
-//     var objDate,  // date object initialized from the ExpiryDate string 
-//         mSeconds, // ExpiryDate in milliseconds 
-//         day,      // day 
-//         month,    // month 
-//         year;     // year 
-//     // date length should be 10 characters (no more no less) 
-//     if (ExpiryDate.length !== 10) { 
-//         return false; 
-//     } 
-//     // third and sixth character should be '/' 
-//     if (ExpiryDate.substring(2, 3) !== '/' || ExpiryDate.substring(5, 6) !== '/') { 
-//         return false; 
-//     } 
-//     // extract month, day and year from the ExpiryDate (expected format is mm/dd/yyyy) 
-//     // subtraction will cast variables to integer implicitly (needed 
-//     // for !== comparing) 
-//     month = ExpiryDate.substring(0, 2) - 1; // because months in JS start from 0 
-//     day = ExpiryDate.substring(3, 5) - 0; 
-//     year = ExpiryDate.substring(6, 10) - 0; 
-//     // test year range 
-//     if (year < 1000 || year > 3000) { 
-//         return false; 
-//     } 
-//     // convert ExpiryDate to milliseconds 
-//     mSeconds = (new Date(year, month, day)).getTime(); 
-//     // initialize Date() object from calculated milliseconds 
-//     objDate = new Date(); 
-//     objDate.setTime(mSeconds); 
-//     // compare input date and parts from Date() object 
-//     // if difference exists then date isn't valid 
-//     if (objDate.getFullYear() !== year || 
-//         objDate.getMonth() !== month || 
-//         objDate.getDate() !== day) { 
-            
-           
-//         return false;; 
-//     } 
-//     // otherwise return true 
-//     return true; 
-// }
